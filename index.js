c42849660e52390021192e42e2aa4725ce960752
Piotr Hołubowicz <piotrekhol@gmail.com>
2020-03-27 05:50:53 +0000

    Added put and delete.

--- index.js
+++ index.js
@@ -1,23 +1,90 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(bodyParser.json()) // for parsing application/json
 
-let games = new Map();
-const game1 = { 'id': 234, 'players': ['Filip', 'Sebas']};
-const game2 = { 'id': 300, 'players': ['Sparta']};
-games.set(234, game1);
-games.set(300, game2);
+var games = new Map();
 
-app.get('/', (req, res) => {
+var nextId = 100;
+
+app.get('/', (_, res) => {
   res.send('Hello Express app!')
 });
 
-app.get('/games', (req, res) => {
-  console.log('/games');
-  res.json(games.values());
+app.get('/games', (_, res) => {
+  res.json([...games.values()]);
 });
 
+app.get('/games/:id', (req, res) => {
+  const game = games.get(+req.params.id);
+  if (!game) {
+    return res.sendStatus(404);
+  }
+  res.json(game);
+});
+
+app.post('/games', (req, res) => {
+  var game = req.body;
+  const error = validateAddGame(game);
+  if (error) {
+    return res.status(400).send(error);
+  }
+  const id = nextId++;
+  game['id'] = id;
+  games.set(id, game);
+  res.json(game);
+});
+
+app.put('/games/:id', (req, res) => {
+  var game = req.body;
+  const id = +req.params.id;
+  const error = validateUpdateGame(game, id);
+  if (error) {
+    return res.status(400).send(error);
+  }
+  if (!games.has(id)) {
+    return res.sendStatus(404)
+  }
+  games.set(id, game);
+  res.sendStatus(200);
+});
+
+app.delete('/games/:id', (req, res) => {
+  const id = +req.params.id;
+  if (!games.has(id)) {
+    return res.sendStatus(404)
+  }
+  delete games.delete(id);
+  res.sendStatus(204);
+});
 
 app.listen(3000, () => {
   console.log('server started');
 });
+
+function validateAddGame(game) {
+  if (typeof game['id'] !== 'undefined') { 
+    return 'You cannot provide an id when adding a new game.\n'
+  };
+  return validateFields(game);
+}
+
+function validateUpdateGame(game, id) {
+  if (typeof game['id'] === 'undefined') { 
+      return 'Field not found: id.\n'
+  };
+  if (game['id'] != id) { 
+      return 'Id in the body and in the path don\'t match.\n'
+  };
+  return validateFields(game);
+}
+
+function validateFields(game) {
+  for(const field of ['players', 'colors', 'hands', 'deck', 'discarded', 'board', 'active_player']) {
+    if (typeof game[field] === 'undefined') { 
+      return 'Field not found: ' + field + '.\n'
+    };
+  }
+  return undefined;
+}
